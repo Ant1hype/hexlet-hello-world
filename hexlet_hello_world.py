b9d2665bdf3084d88cbd2760b7daf2eb2d2cfc07
Ant1hype <idiotovabdulf@yandex.ru>
2026-02-05 15:23:48 +0700

    trying to configure pyproject.toml through

--- hexlet_hello_world.py
+++ hexlet_hello_world.py
@@ -1,1 +1,8 @@
+from more_itertools import flatten, substrings
+
+coll = [(0, 1), ((2, 3))]
+print(list(flatten(coll)))
+
+print([''.join(s) for s in substrings('more')])
+
 print("Hello from python project")
